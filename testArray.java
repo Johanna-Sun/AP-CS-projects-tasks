2a7aafaa78ff7f6ac7e745e0a4845b02351a0d7b
JohannaSun <sunyx.johanna2016@outlook.com>
2018-01-25 21:39:09 +0800

    preparation

--- testArray.java
+++ testArray.java
@@ -1,9 +1,19 @@
+import java.util.*;
+
 public class testArray {
 
   public static void main(String[] args) {
 
-    double test = 5.0/3;
-    System.out.println(test);
+    // double test = 5.0/3;
+    // System.out.println(test);
+
+    ArrayList<Integer> test = new ArrayList<Integer>();
+    test.add(1);
+    test.add(2);
+    test.add(3);
+    test.remove(0);
+    test.set(0,9);
+    System.out.println(test.get(0));
 
   }
 
